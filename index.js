c97c93931dcdf5ab1f09b5cb4d22a5ede6d2858d
Mayza <mayzamrodrigues@gmail.com>
2025-08-07 13:48:49 -0300

    release: first version of the website

--- index.js
+++ index.js
@@ -1,9 +1,59 @@
-document.getElementById("form").addEventListener("submit", function () {
-  const mensagem = document.getElementById("mensagem");
-  mensagem.style.display = "block";
-  setTimeout(() => {
-    mensagem.style.display = "none";
-    const input = document.getElementById("emailInput");
-    input.value = ""
-  }, 2000);
+
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("nav");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
+
+const booking = document.querySelector(".intro__booking")
+if (booking){
+booking.innerHTML += booking.innerHTML
+}
+
+
+const menuIcon = document.getElementById("menu-icon");
+console.log(menuIcon)
+const menu = document.querySelector(".nav__menu")
+
+if (menuIcon && menu){
+
+menuIcon.addEventListener("click", function () {
+  console.log("Menu icon clicked");
+  menu.classList.toggle("active")
+
+   if (menu.classList.contains("active")) {
+            menuIcon.textContent = "close";
+        } else {
+            menuIcon.textContent = "menu";
+        }
+})
+
+}
+
+    const headers = document.querySelectorAll('.accordion-header');
+
+    headers.forEach(header => {
+      header.addEventListener('click', () => {
+
+        const content = header.nextElementSibling;
+        const arrow = header.querySelector('.arrow');
+
+        if (content.style.display === 'block') {
+          content.style.display = 'none';
+          arrow.textContent = '▼';
+        } else {
+          content.style.display = 'block';
+          arrow.textContent = '▲';
+
+        }
+      });
+    });
+
+
+
+
+
+
